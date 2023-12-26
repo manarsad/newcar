930e6dcac8436c0099b6908af1b8f76bd21ca70c
manarsadaqa <148491866+manarsadaqa@users.noreply.github.com>
2023-12-26 20:01:01 +0300

    Update Installationrequest.java

--- src/main/java/caraccessories/Installationrequest.java
+++ src/main/java/caraccessories/Installationrequest.java
@@ -10,60 +10,6 @@ public class Installationrequest {
 	private String day ;
 	private String month;
 	private int hour;
-		
-	public int getRequestid() {
-		return requestid;
-	}
-	public void setRequestid(int requestid) {
-		this.requestid = requestid;
-	}
-	public int getProductid() {
-		return productid;
-	}
-	public void setProductid(int productid) {
-		this.productid = productid;
-	}
-	
-	public String reqproductname() {
-		return reqproductname;
-	}
-	public void setreqproductname(String reqproductname) {
-		this.reqproductname = reqproductname;
-	}
-	public String reqinstallername() {
-		return reqinstallername;
-	}
-	public void setreqinstallername(String reqinstallername) {
-		this.reqinstallername =reqinstallername;
-	}
-	
-	public String reqcostumername() {
-		return reqcostumername;
-	}
-	public void setreqcostumername(String reqcostumername) {
-		this.reqcostumername = reqcostumername;
-	}
-	
-	public String getDay() {
-		return day;
-	}
-	public void setDay(String day) {
-		this.day = day;
-	}
-	public String getMonth() {
-		return month;
-	}
-	public void setMonth(String month) {
-		this.month = month;
-	}
-	
-	public int getHour() {
-		return hour;
-	}
-	public void setHour(int hour) {
-		this.hour = hour;
-	}
-	
 	public Installationrequest( int requestid  , String reqinstallername,int productid ,String reqproductname, String reqcostumername , String day , String month , int hour) {
 		this.requestid=requestid;
 		this.reqinstallername= reqinstallername;
