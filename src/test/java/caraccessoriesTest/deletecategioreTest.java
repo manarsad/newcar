6d31210a9b54516c9f866e316f0e0df2fdae2c6e
manarsadaqa <148491866+manarsadaqa@users.noreply.github.com>
2023-12-26 22:01:52 +0300

    Update deletecategioreTest.java

--- src/test/java/caraccessoriesTest/deletecategioreTest.java
+++ src/test/java/caraccessoriesTest/deletecategioreTest.java
@@ -13,7 +13,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class deletecategioreTest {
+public class deleteCategioreTest {
 	private Product product = new Product();
     private static ProductFun productfun = new ProductFun();
 
