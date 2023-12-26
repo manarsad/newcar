982d2363d80325b38e1e4bf413c2c68076e06f9f
manarsadaqa <148491866+manarsadaqa@users.noreply.github.com>
2023-12-26 21:32:57 +0300

    Update deleteproductTest.java

--- src/test/java/caraccessoriesTest/deleteproductTest.java
+++ src/test/java/caraccessoriesTest/deleteproductTest.java
@@ -1,6 +1,7 @@
 
 package caraccessoriesTest;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Assert;
@@ -28,7 +29,7 @@ public class deleteproductTest {
 	@Then("I will found the product with productid as {int}")
 	public void i_will_found_the_product_with_productid_as(Integer int1) {
 		//true.. delete 
-		assertTrue(ProductFun.getflag3()==true);
+	    assertEquals(true, ProductFun.getflag3());
 
 	   //  assertFalse(productfun.getproductid1(int1)!=null);		
 		 assertTrue(d);
@@ -44,8 +45,7 @@ public class deleteproductTest {
 	}
 	@Then("I can't found the product with  a productid {int}")
 	public void i_can_t_found_the_product_with_a_productid(Integer int1) {
-		assertTrue(ProductFun.getflag2()==false);
-
+	    assertEquals(false, ProductFun.getflag3());
 		//assertFalse(productfun.getproductid1(int1)!=null);		
 		assertFalse(d);
 
