327b79d996084dbebee2a1798c0c59d1eaeb159d
manarsadaqa <148491866+manarsadaqa@users.noreply.github.com>
2023-12-26 04:33:14 +0300

    Update InstallationrequestFun.java

--- src/main/java/caraccessories/InstallationrequestFun.java
+++ src/main/java/caraccessories/InstallationrequestFun.java
@@ -17,18 +17,7 @@ public class InstallationrequestFun {
 
 	public static boolean getflag1() {
 		return flag1;
-	}
-	public static boolean getflag2() {
-		return flag2;
-	}
-	public static boolean getflag3() {
-		return flag3;
-	}
-	public static boolean getisSented() {
-		return isSented;
-	}
-	
-
+	}	
 
 	static {
 		Installationrequest request1 = new Installationrequest(1,"ali",1,"vehicle engine", "sama","Sunday","14-April",12);
@@ -36,20 +25,12 @@ public class InstallationrequestFun {
 		requestList.add(request1);
 		requestList.add(request2);
 	}
-	
-	
         
 	public List<Installationrequest> request( int requestid  , String reqinstallername,int productid ,String reqproductname, String reqcostumername , String day , String month , int hour ) {
-	    // Check if the product exists in the productList
-		    // If the product exists, add it to the orderList
-		    //for (Installationrequest request : requestList) {
-		      //  if (request.getRequestid() == productid) {
 		            requestList.add(new Installationrequest(requestid,reqinstallername,productid ,reqproductname,reqcostumername ,day , month ,hour));
 			        System.out.println("done .. add it to installation Request list ");
 			        sendEmailConfirmation(reqcostumername,reqproductname);
 			        flag1=true;
-		      //  }
-		//    }
             return requestList;
 
 	}
@@ -60,19 +41,19 @@ public class InstallationrequestFun {
         }
     }
 	
-	public static void sendEmailConfirmation(String costumername ,String productname) {
-		String from = "s12027821@stu.najah.edu";
-		String to = "s12011151@stu.najah.edu";
-		
-		String subject = "NewInstallation Request";
-		String messageText = " Dear Installer, \n \n You have a new installation request. \n \n "
-				+ "Customer:"+ costumername + "\n"
-				+" Product :" + productname +"$" + "\n \n" 
-				+ "Please check your dashboard for more details.";
-		isSented=true; 
-		EmailSender.sendEmail(from, to, subject, messageText);
-		
-				}
+	
+    public static void sendEmailConfirmation(String customerName, String productName) {
+        String from = "s12027821@stu.najah.edu";
+        String to = "s12011151@stu.najah.edu";
+
+        String subject = "New Installation Request";
+        String messageText = "Dear Installer, \n \n You have a new installation request. \n \n "
+                + "Customer:" + customerName + "\n"
+                + " Product :" + productName + "$" + "\n \n"
+                + "Please check your dashboard for more details.";
+      //  isSented = true;
+        EmailSender.sendEmail(from, to, subject, messageText);
+    }
 	
 	
 
